0cfacea823ae5fca649a075c97459115c1dcf6ee
rare1k <86028086+uhidontkno@users.noreply.github.com>
2024-01-30 18:46:19 -0500

    add rate limit + other shit

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
 import { Elysia } from "elysia";
 import config from "./config";
-
+import { rateLimit } from "elysia-rate-limit";
 const server = new Elysia();
+server.use(rateLimit({duration: 30000, max:100, responseMessage:"Global rate limit reached"}))
 
 server.get("/", () => {
     return "Welcome to Deblok!";
