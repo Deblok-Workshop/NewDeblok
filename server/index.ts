29b47a243fa729fd6d80d8cb4ca2a8b7f078019a
rare1k <86028086+uhidontkno@users.noreply.github.com>
2024-01-30 18:48:42 -0500

    typo lmao

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 import config from "./config";
 import { rateLimit } from "elysia-rate-limit";
 const server = new Elysia();
-server.use(rateLimit({duration: 30000, max:100, responseMessage:"Global rate limit reached"}))
+server.use(rateLimit({duration: 300000, max:100, responseMessage:"Global rate limit reached"}))
 
 server.get("/", () => {
     return "Welcome to Deblok!";
