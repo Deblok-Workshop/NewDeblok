50999afd2f78cc726213452847cc985b31c3a094
rare1k <86028086+uhidontkno@users.noreply.github.com>
2024-01-31 16:25:01 -0500

    static webserver & minor stuff

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,19 @@
 import { Elysia } from "elysia";
 import config from "./config";
 import { rateLimit } from "elysia-rate-limit";
+import { staticPlugin } from '@elysiajs/static'
+import { cors } from '@elysiajs/cors'
+
 const server = new Elysia();
+
 server.use(rateLimit(config.ratelimit))
-server.get("/", () => {
+server.use(staticPlugin({assets:"static/",prefix:"/"}))
+server.use(cors()) // ElysiaJS cors plugin
+
+server.get("/api/", () => {
     return "Welcome to Deblok!";
+})
+server.get("/api/__healthcheck", () => { // using /api/__healthcheck to be compatible with Kasmweb's API format
+  return {healthy:true};
 })
 server.listen(config.webserver);
