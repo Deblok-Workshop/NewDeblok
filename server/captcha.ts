ef50dfd9bec39c4e9d8c960edfda75e1e46d593d
rare1k <86028086+uhidontkno@users.noreply.github.com>
2024-02-02 23:47:51 -0500

    captcha + test api endpoint

--- server/captcha.ts
+++ server/captcha.ts
@@ -1,4 +1,46 @@
+import sharp from 'sharp';
+function translateMathFuck(mathFuckCode: string): string {
+    const translation: {[key: string]: string} = {
+        'ඞ?': '+',
+        'ඞ.': '-',
+        'ඞ-': '×',  
+        'ඞ;': '/',
+        'ඞ!': '^',
+        'ඞ=': '√',
+        'ඞ#': '=',
+        '_ඞ': '(',
+        'ඞ_': ')'
+    };
+    return mathFuckCode.replaceAll(/ඞ[?.;!=#_-]|_ඞ/g, match => translation[match] || match);
+}
+
+function shift(inputString: string, flipbit: number = 1) {
+    let shiftedString = '';
+    if (flipbit == -1 || flipbit == 1) {} else {flipbit = 1}
+    for (let i = 0; i < inputString.length; i++) {
+        const codePoint = inputString.charCodeAt(i);
+        const shiftedCodePoint = codePoint + (54*flipbit);
+        shiftedString += String.fromCharCode(shiftedCodePoint);
+    }
+    
+    return shiftedString;
+}
 
 
+async function makemfk_img(mfk: string,dense: number = 72): Promise<Buffer> {
+    const readExp = translateMathFuck(mfk);
+    let svg = Bun.file('captchabase.svg')
+    let svgStr = await svg.text();
+    const lines = svgStr.split('\n');
+    lines[6] = `${readExp}`;
+    svgStr = lines.join('\n');
+    const gifBuffer = await sharp(Buffer.from(svgStr), { density: dense })
+        .toFormat('gif')
+        .toBuffer();
+    return gifBuffer;
+}
 
-export default {}
+
+export default {
+    mathfuck:{translate:translateMathFuck,img:makemfk_img,shift:shift}
+}
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
 import config from "./config";
 import helper from "./helper";
+import captcha from "./captcha";
 import { rateLimit } from "elysia-rate-limit";
 import { staticPlugin } from '@elysiajs/static'
 import { cors } from '@elysiajs/cors'
@@ -17,5 +18,17 @@ server.get("/api/", () => {
 server.get("/api/__healthcheck", () => { // using /api/__healthcheck to be compatible with Kasmweb's API format
   return {};
 })
+server.get("/api/testing/captcha", async ({ params: { dense } }) => {
+  try {
+      const imageBuffer = await captcha.mathfuck.img('ඞ=_ඞ6ඞ-6ඞ_ඞ?_ඞ6ඞ-6ඞ_ඞ?3ඞ.3',72);
+      return new Blob([imageBuffer]);
+  } catch (error) {
+      console.error('Error generating captcha:', error);
+      return {
+          status: 500,
+          error: error,
+      };
+  }
+});
 
 server.listen(config.webserver);
