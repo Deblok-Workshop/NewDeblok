5f31ad0a574c7e40be0dc7471e6996509a20f7f6
rare1k <86028086+uhidontkno@users.noreply.github.com>
2024-02-04 22:27:18 -0500

    Add TODO for later

--- server/index.ts
+++ server/index.ts
@@ -74,6 +74,38 @@ server.get("/api/captcha/request", async () => {
   Bun.write('./tempcaptcha.db',JSON.stringify(tempdb))
   return randuuid
  });
+server.post("/api/auth/signup", async ({body,set}) => {
+  const b:any=body // the body variable is actually a string, this is here to fix a ts error
+  const bjson:Object=JSON.parse(b)
+// TODO:
+// Read the body JSON (which is the bjson variable), then
+// Accept a sha256 hash (must start with "sha256:") for the password
+// then securely hash that using Bun.password.hash for secure double
+// hashing. Once it is hashed, add the entry to the hcreds table in
+// the SQL database. The username will be MD5 hashed in the db and
+// will be used as a user ID. The entry name will be the uid, and
+// the entry value will be the double hashed password.
+
+// Use helper.sql.open('db.sql') to open the database
+// Use helper.sql.read(db, 'tablename','name') to read
+// Use helper.sql.write(db,'tablename','name','value') to write
+// Use helper.crypto.aes.encrypt(mode,'text','key') to AES encrypt
+// Use helper.crypto.aes.decrypt(mode,'text','key') to AES decrypt
+
+// Do something similar for the login endpoint. 
+// I don't feel like writing a whole another paragraph rn bc 
+// im on mobile and i need to fix ssh 😞
+ });
+
+server.post("/api/auth/login", async ({body,set}) => {
+  const b:any=body // the body variable is actually a string, this is here to fix a ts error
+  const bjson:Object=JSON.parse(b)
+// Please see the TODO message above to see what to do.
+// The login endpoint will be similar. Please read Bun docs to
+// find out what the argon hash validation function is because
+// I don't know it off of the top of my head.
+
+}
 console.log(`Listening on port ${config.webserver.port} or`)
 console.log(` │ 0.0.0.0:${config.webserver.port}`)
 console.log(` │ 127.0.0.1:${config.webserver.port}`)
