84269a7ee51f7700c65090834e2be99ae4e84004
uhidontkno <hi@rare1k.dev>
2024-02-09 06:44:14 -0500

    I forgot to add hash checking

--- server/index.ts
+++ server/index.ts
@@ -115,9 +115,11 @@ server.post("/api/auth/signup", async ({body,set}) => {
   if (usr == "" || !usr || pwd == "" || !pwd || email == "" || !email) {
     set.status = 400; return "ERR: One or more fields are missing."
   }
+  if (pwd.length != 71) {set.status = 400; return "ERR: Invalid input."}
+  if (usr.length != 36) {set.status = 400; return "ERR: Invalid input."}
   if (!String(pwd).startsWith('sha256:') || !String(usr).startsWith('md5:')) {
     set.status = 400; return "ERR: Invalid input."
-    // TODO: actually validate the hashes
+    
   }
   try {
   var db = helper.sql.open('db.sql',true)
