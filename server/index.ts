a8cc54f0420266ace901fbec2e71ab8eae1795bf
uhidontkno <hi@rare1k.dev>
2024-02-09 16:26:57 -0500

    sha256 hash the db password for security

--- server/index.ts
+++ server/index.ts
@@ -21,8 +21,9 @@ server.use(cors()) // ElysiaJS cors plugin
 // initial startup stuff
 let endpoints:any = process.env.ENDPOINTS
 if (!endpoints) {throw new ReferenceError('Cannot find Neko Rooms endpoints (check .env)')}
-let dbpwd = process.env.DBPWD
-if (!dbpwd) {throw new ReferenceError('No Database Password')}
+let dbpwd:any = process.env.DBPWD
+dbpwd = new Bun.CryptoHasher('sha256').update(dbpwd).digest('hex')
+if (!dbpwd) {throw new ReferenceError('No Database Password (check .env)')}
 endpoints = endpoints.split(',')
 
 // general
