4045e32ef37edcf1a5ef2d59671c16f7d0c513b6
incognitotgt <mashoor539@icloud.com>
2024-02-11 01:15:54 -0500

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 export default {
-  content: ["./static/**/*.html"],
+  content: ["./static/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
