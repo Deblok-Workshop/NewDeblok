107353e69441beaddc6e7952ec5c06b09dd85d99
uhidontkno <hi@rare1k.dev>
2024-02-11 16:29:59 -0500

    make the shittiest theme

--- static/global.js
+++ static/global.js
@@ -1,6 +1,11 @@
 function updateTheme() {
-    if (localStorage['themeSelected'] != undefined) {
+    if (localStorage['themeSelected'] == "horrible latte") {
+        document.querySelector('html').className = "latte"
+        document.querySelector('body').classList.add('horrible')
+    }
+    else if (localStorage['themeSelected'] != undefined) {
         document.querySelector('html').className = (localStorage['themeSelected'])
+        document.querySelector('body').classList.remove('horrible')
     }
 }
 updateTheme()
