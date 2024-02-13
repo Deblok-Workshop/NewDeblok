95e0f9153d8dadc9513e83acb193ad508e44e982
uhidontkno <hi@rare1k.dev>
2024-02-12 21:34:44 -0500

    actually handle successful captchas

--- static/auth.js
+++ static/auth.js
@@ -1,6 +1,9 @@
 function checkCaptchaIfr(ele) {
   let doc = ele.contentWindow.document || ele.contentDocument;
-  return /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(
-    doc.body.innerHTML,
-  );
+  let rgt =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(
+      doc.body.innerHTML,
+    );
+  console.log(rgt);
+  return rgt;
 }
