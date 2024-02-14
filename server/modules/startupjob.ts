e9db5047a49912c0dc36f1815ad08227cdac2259
rare1k <hi@rare1k.dev>
2024-02-14 16:05:06 -0500

    Update startupjob.ts

--- server/modules/startupjob.ts
+++ server/modules/startupjob.ts
@@ -1,4 +1,19 @@
 if (
+  process.argv.includes("--license") ||
+  process.argv.includes("--copyright") ||
+  process.argv.includes("--copying")
+) {
+  console.log(`
+NewDeblok is licensed under the GNU General Public License v3.0 (GPL-3.0)
+You can find the full license text in the LICENSE file or at https://opensource.org/license/gpl-3-0/
+
+- md5.js (https://pajhome.org.uk/crypt/md5/) is distributed under the BSD license, available at https://opensource.org/license/bsd-3-clause/
+Please see https://pajhome.org.uk/site/legal.html#bsdlicense for more information.
+    `);
+  process.exit(0);
+}
+
+if (
   process.argv.includes("--help") ||
   process.argv.includes("-?") ||
   process.argv.includes("-h")
@@ -43,16 +58,3 @@ if (!process.env.DBPWD) {
 }
 endpoints = endpoints.split(",");
 
-if (
-  process.argv.includes("--license") ||
-  process.argv.includes("--copyright") ||
-  process.argv.includes("--copying")
-) {
-  console.log(`
-NewDeblok is licensed under the GNU General Public License v3.0 (GPL-3.0)
-You can find the full license text in the LICENSE file or at https://opensource.org/license/gpl-3-0/
-
-md5.js (https://pajhome.org.uk/crypt/md5/) is distributed under the BSD license, available at https://opensource.org/license/bsd-3-clause/
-    `);
-  process.exit(0);
-}
