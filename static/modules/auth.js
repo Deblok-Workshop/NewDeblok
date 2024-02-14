6251508d13cb1ff70b382ad63f3fc1885fcb4945
uhidontkno <hi@rare1k.dev>
2024-02-13 20:50:11 -0500

    add hashing functions

--- static/modules/auth.js
+++ static/modules/auth.js
@@ -1,3 +1,26 @@
+async function md5Hash(input) {
+  // TODO
+}
+
+async function sha256Hash(input) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 function checkCaptchaIfr(ele) {
   let doc = ele.contentWindow.document || ele.contentDocument;
@@ -60,3 +83,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
