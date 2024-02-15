62c4874212c7f4a013e9cb90257e3ccb1e3327a6
uhidontkno <hi@rare1k.dev>
2024-02-14 19:47:04 -0500

    add setting up wordlists

---
+++ scripts/wordlist_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git clone https://github.com/Deblok-Workshop/Wordlists
+sleep 1 # It will not work without this
+if [ "$1" != "--minimal" ]; then
+bash ./Wordlists/rockyou.sh --gunzip
+cp rockyou.txt ./Wordlists/rockyou.txt
+rm -rf rockyou.txt
+fi
+cp ./Wordlists/*.txt ../server/modules/wordlists/
+sleep 1
+rm -rf ./Wordlists
+sleep 1 
+echo Done!
