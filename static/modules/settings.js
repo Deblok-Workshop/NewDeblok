3e8e7ca06ed5b7f6b4f3583a5aa781707f8d761b
uhidontkno <hi@rare1k.dev>
2024-02-17 17:07:35 -0500

    The quick brown fox jumped over the lazy dog. (actually make quartz's code)

--- static/modules/settings.js
+++ static/modules/settings.js
@@ -27,30 +27,3 @@ function cloakTab(iconUrl, title) {
   document.getElementsByTagName('head')[0].appendChild(l);
   document.title = title;
 }
-
-let emergencyButtonListener = null;
-
-function emergencyButtonCheck(checkboxElement) {
-  if (checkboxElement.checked) {
-    if (emergencyButtonListener) {
-      document.removeEventListener("keydown", emergencyButtonListener);
-    }
-    const action = document.querySelector('.enableEmergencyButton + .if-section select').value;
-    const keybind = document.querySelector('.emergencyButtonKeybind').value.toUpperCase();
-    const requireShift = document.querySelector('.requireShift').checked;
-    const requireCtrl = document.querySelector('.requireCtrl').checked;
-    emergencyButtonListener = function (event) {
-      if ((requireShift ? event.shiftKey : true) && (requireCtrl ? event.ctrlKey : true) && event.key.toUpperCase() === keybind) {
-        if (action === 'Close Tab') {
-          window.close();
-        } else if (action === 'Redirect to Safe Page') {
-          window.location.href = "https://docs.google.com/";
-        }
-      }
-    };
-    document.addEventListener("keydown", emergencyButtonListener);
-  } else if (emergencyButtonListener) {
-    document.removeEventListener("keydown", emergencyButtonListener);
-    emergencyButtonListener = null;
-  }
-}
