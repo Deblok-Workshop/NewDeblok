216eaeebd721c714d5f6ea57508f05492fad2394
uhidontkno <hi@rare1k.dev>
2024-02-17 16:55:24 -0500

    quartz is such a skid, fix his shit code

--- static/modules/auth.js
+++ static/modules/auth.js
@@ -127,3 +127,21 @@ async function signup(usr, pwd, em) {
     return undefined;
   }
 }
+
+
+async function loginForm() {
+  let usrEle = document.querySelector('input[type="username"]');
+  let usrPwd = document.querySelector('input[type="password"]');
+  let res = await login(usrEle.value,usrPwd.value)
+  if (res != undefined) {
+    if (res.ok) {
+      localStorage['DEBLOKAUTH'] = res.text()
+      document.location = new URLSearchParams(window.location.search).get('redirect_to') || "/dash.html"
+    } else {
+      alert(await res.text())
+      usrPwd.value = ""
+      usrEle.value = ""
+  }
+  }
+  return res
+}
