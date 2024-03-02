e262901c11a1bddce92f712194cfcc7b04104b50
uhidontkno <hi@rare1k.dev>
2024-03-01 22:59:15 -0500

    button callback

--- static/modules/dash.js
+++ static/modules/dash.js
@@ -55,7 +55,7 @@ function itemModal(title, description, icon,buttons = ["OK","Cancel"]) {
             <button
               type="button"
               class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-              onclick="itemModalHide()"
+              onclick="window.btnReturn = 'closed';itemModalHide();"
             >
               <svg
                 class="w-3 h-3"
@@ -84,7 +84,7 @@ function itemModal(title, description, icon,buttons = ["OK","Cancel"]) {
             class="bg-black/20 flex items-center p-4 md:p-5 space-x-3 rtl:space-x-reverse border-t border-black/40 rounded-b"
           >
             <button
-              data-modal-hide="item-modal"
+              onclick="window.btnReturn = 'clickedbtn0';itemModalHide();"
               type="button"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center s:ring-blue-800"
             >
@@ -92,8 +92,8 @@ function itemModal(title, description, icon,buttons = ["OK","Cancel"]) {
             </button>
             ${buttons[1] ? `
             <button
-              data-modal-hide="item-modal"
               type="button"
+              onclick="window.btnReturn = 'clickedbtn1';itemModalHide();"
               class="py-2.5 px-5 ms-3 text-sm font-medium text-text focus:outline-none bg-base rounded-lg border border-black/40 hover:bg-surface0 hover:text-blue focus:z-10 focus:ring-4 focus:ring-gray-100 s:ring-gray-700"
             >
               ${buttons[1]}
