f1055cbefd225660107d00f83105afd637b92ada
uhidontkno <hi@rare1k.dev>
2024-03-01 23:08:23 -0500

    fix auth validation

--- server/modules/helper.ts
+++ server/modules/helper.ts
@@ -152,7 +152,9 @@ function auth_tokenvalidate_endpoint(body: any) {
     let crtime = decodeauthpart(authtoken[1].substring(2));
 
     let now = Date.now();
-    if (now > Number(exptime) || now > Number(crtime)) {
+    console.log(exptime, crtime, now)
+    if (now < Number(exptime) && now > Number(crtime)) {
+      // exptime should be in the future, crtime should be in the past.
       // All good
     } else {
       error = true;
