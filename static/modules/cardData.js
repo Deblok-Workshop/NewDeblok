ed8db068fc99910108bb5fa5b77c0b6834100cbf
uhidontkno <hi@rare1k.dev>
2024-03-05 18:36:58 -0500

    add descriptions for apps

--- static/modules/cardData.js
+++ static/modules/cardData.js
@@ -4,42 +4,49 @@ const cardData = [
     title: "Firefox",
     subtitle: "Internet",
     name: "firefox",
+    description: "Firefox on Deblok."
   },
   {
     img: "assets/icons/chromium.png",
     title: "Chromium",
     subtitle: "Internet",
     name: "chromium",
+    description: "Chromium on Deblok."
   },
   {
     img: "assets/icons/opera.png",
     title: "Opera",
     subtitle: "Internet",
     name: "opera",
+    description: "Opera on Deblok."
   },
   {
     img: "assets/icons/gimp.png",
     title: "GIMP",
     subtitle: "Multimedia",
     name: "gimp",
+    description: "GIMP on Deblok."
   },
   {
     img: "assets/icons/ubuntu-xfce.png",
     title: "Ubuntu (XFCE)",
     subtitle: "Desktop",
     name: "ubuntu-xfce",
+    description: "Ubuntu (XFCE) on Deblok."
   },
   {
     img: "assets/icons/fedora-xfce.png",
     title: "Fedora (XFCE)",
     subtitle: "Desktop",
     name: "fedora-xfce",
+    description: "Fedora (XFCE) on Deblok."
   },
   {
     img: "assets/icons/debian-xfce.png",
     title: "Debian (XFCE)",
     subtitle: "Desktop",
     name: "debian-xfce",
+    description: "Debian (XFCE) on Deblok."
   },
   // Made by spark and rare pookie
 ];
