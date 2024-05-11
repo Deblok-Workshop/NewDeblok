c220abd09c5d2fa5d16bff891b97adc58b8516c6
rare1k <hi@rare1k.dev>
2024-05-11 13:53:07 -0400

    feat: increase rate limit

--- server/config.ts
+++ server/config.ts
@@ -2,7 +2,7 @@ export default {
   // Ratelimit config
   ratelimit: {
     windowMs: 300 * 1000,
-    limit: 64,
+    limit: 100,
     skipFailedRequests: true,
     message: "Global rate limit reached.",
     skip: (req: any) => {
