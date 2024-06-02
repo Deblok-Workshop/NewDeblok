636b5ae7a4a8586e7393ec39bf0e8539e24ab71b
rare1k <hi@rare1k.dev>
2024-06-02 17:49:31 -0400

    feat: init material 3 and tw

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import "../app.css"
+    import { StyleFromScheme } from "m3-svelte";
+</script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" />
+<slot />
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+    import { Button } from "m3-svelte";
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button type="filled" on:click={() => alert("Hello world!")}>Hello world!</Button>
