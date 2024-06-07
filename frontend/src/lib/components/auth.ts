496e726ee359c5d8f26c460deccb8143ad0a4bea
rare1k <hi@rare1k.dev>
2024-06-07 17:19:26 -0400

    feat: respawn form automatically on fail

--- frontend/src/lib/components/auth.ts
+++ frontend/src/lib/components/auth.ts
@@ -9,12 +9,17 @@ async function onAuthClick() {
 	let usr = document.querySelector("input[type=\"username\"]").value
 	if (pwd.trim() == "" || usr.trim() == "") {
 		toast.error("All fields are required.");
+		// @ts-expect-error
+		document.querySelector('.auth-dialog-trigger').click();
 		return
 	}
 	if (!passwordRegex.test(pwd)) {
 		toast.error("Password does not meet requirements.", {
 			description: "A password must be 10 characters long, have atleast 1 lowercase letter, and atleast 1 number.",
-		  });return;
+		  });
+		// @ts-expect-error
+		document.querySelector('.auth-dialog-trigger').click();
+		  return;
 	}
 }
 
